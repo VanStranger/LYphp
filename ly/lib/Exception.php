5b110d28932020f34cb697ba5af96fc96656f01e
成功那么难吗 <fanjunjiei@126.com>
2021-08-01 18:08:15 +0800

    小更新

--- ly/lib/Exception.php
+++ ly/lib/Exception.php
@@ -271,7 +271,7 @@ class Exception{
            $title = $type == 'db' ? 'Database' : 'System';
         ?>
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html>
+        <html lang="zh">
         <head>
             <title><?php echo $host."-".$title;?> Error</title>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
--- ly/lystart.php
+++ ly/lystart.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
 date_default_timezone_set('PRC');
-define("LY_BASEPATH", dirname(__DIR__)."/");
-chdir(dirname(__DIR__)."/public");
+define("LY_BASEPATH", dirname(__DIR__).DIRECTORY_SEPARATOR);
+chdir(dirname(__DIR__).DIRECTORY_SEPARATOR."public");
 !defined("APP_PATH") && define("APP_PATH","application");
 include LY_BASEPATH."ly/lib/Loader.php";
 include LY_BASEPATH."ly/help.php";
